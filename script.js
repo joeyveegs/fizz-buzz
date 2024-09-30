2ba4a2039010b67d66d277cfb3125b55d968352d
joeyveegs <64642147+joeyveegs@users.noreply.github.com>
2024-09-30 14:15:39 +0100

    Finish basic code by writing if else statement

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let userNumber = parseInt(prompt("Enter a number you'd like to FizzBuzz up to:"));
 
 for (let i = 1; i <= userNumber; i++) {
-    console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz");    
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else {
+    console.log(i);        
+    }
 }
